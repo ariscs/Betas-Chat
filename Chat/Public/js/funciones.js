4a1a9c4ec548976425dee0f487dc7f8d3181f7f9
Aristides Camacho <acamachos11@outlook.com>
2018-12-03 20:40:35 -0600

    se juntaron los .html
    ya esta todo el menu en un solo html y funcionando

--- Chat/Public/js/funciones.js
+++ Chat/Public/js/funciones.js
@@ -1,3 +1,37 @@
+function nav(id){
+	switch(id){
+		case "contactsButton":
+			document.getElementById('chats').style.display = "none";
+			document.getElementById('chatsButton').src = "img/speechGray.svg";
+			document.getElementById('config').style.display = "none";
+			document.getElementById('configButton').src = "img/settingsGray.svg";
+			document.getElementById('contacts').style.display = "initial";
+			document.getElementById('contactsButton').src = "img/phone-bookBlue.svg";
+			break;
+		case "chatsButton":
+			document.getElementById('chats').style.display = "initial";
+			document.getElementById('chatsButton').src = "img/speechBlue.svg";
+			document.getElementById('config').style.display = "none";
+			document.getElementById('configButton').src = "img/settingsGray.svg";
+			document.getElementById('contacts').style.display = "none";
+			document.getElementById('contactsButton').src = "img/phone-bookGray.svg";
+			break;
+		case "configButton":
+			document.getElementById('chats').style.display = "none";
+			document.getElementById('chatsButton').src = "img/speechGray.svg";
+			document.getElementById('config').style.display = "initial";
+			document.getElementById('configButton').src = "img/settingsBlue.svg";
+			document.getElementById('contacts').style.display = "none";
+			document.getElementById('contactsButton').src = "img/phone-bookGray.svg";
+			break;
+		default:
+			console.log("Error en funcion nav()");
+			break;
+	}
+}
+
+
+//Funciones Jquery
 $( ".toggle-switch" ).click(function() {
 	if ($("#RepetirContraseña").length) {
 		$('#RepetirContraseña').remove();
