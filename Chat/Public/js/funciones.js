8ce08f78a2e071d40be824caaf800b4d97ea5ae8
Aristides Camacho <acamachos11@outlook.com>
2018-12-03 18:46:58 -0600

    Chat Room
    plantilla del chat, faltan detalles pero en escencia esta asi se estaria viendo

--- Chat/Public/js/funciones.js
+++ Chat/Public/js/funciones.js
@@ -1,33 +1,22 @@
 $( ".toggle-switch" ).click(function() {
-
 	if ($("#RepetirContraseña").length) {
-		
 		$('#RepetirContraseña').remove();
 		$('#MandarCorreo').replaceWith('<input type="text" class="inputDatos" id="Correo" placeholder="Correo electronico">');
 		$('#MandarContraseña').replaceWith('<input type="text" class="inputDatos" id="Contraseña" placeholder="Contraseña">');
-
-	}
-		else{
-			$('#login-button').replaceWith('<input type="submit" id="registro-button" class ="mandar" value="Registrar">');
-				
-			$('.mandar').before('<input type="text" class="inputDatos" id="RepetirContraseña" placeholder="Repita la Contraseña">');
-			$('#Correo').replaceWith('<input type="text" class="inputDatos" id="MandarCorreo" placeholder="Correo electronico">');
-			$('#Contraseña').replaceWith('<input type="text" class="inputDatos" id="MandarContraseña" placeholder="Contraseña">');
-		}
-	
-
-	
-	
-		});
+	}else{
+		$('#login-button').replaceWith('<input type="submit" id="registro-button" class ="mandar" value="Registrar">');		
+		$('.mandar').before('<input type="text" class="inputDatos" id="RepetirContraseña" placeholder="Repita la Contraseña">');
+		$('#Correo').replaceWith('<input type="text" class="inputDatos" id="MandarCorreo" placeholder="Correo electronico">');
+		$('#Contraseña').replaceWith('<input type="text" class="inputDatos" id="MandarContraseña" placeholder="Contraseña">');
+	}	
+});
 
 
 $(document).ready(function() {
-
-$("#Principal").mousemove(function(e){
-          var pageX = (e.pageX* -1 /15) ;
-          var pageY = (e.pageY* -1 /15) ;
-       
-          $('#Principal').css("background-position", pageX+"px     "+pageY+"px");
-});
+	$("#Principal").mousemove(function(e){
+		var pageX = (e.pageX* -1 /15) ;
+		var pageY = (e.pageY* -1 /15) ;
+		$('#Principal').css("background-position", pageX+"px     "+pageY+"px");
+	});
 });
 
