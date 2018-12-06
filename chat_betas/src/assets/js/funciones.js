19dafecbb96bc336a6a2e724172946aa0f13a542
Aristides Camacho <acamachos11@outlook.com>
2018-12-06 17:31:21 -0600

    correccion media query

--- chat_betas/src/assets/js/funciones.js
+++ chat_betas/src/assets/js/funciones.js
@@ -1,33 +1,44 @@
 function nav(id){
-	switch(id){
-		case "contactsButton":
-			document.getElementById('chats').style.display = "none";
-			document.getElementById('chatsButton').src = "img/speechGray.svg";
-			document.getElementById('config').style.display = "none";
-			document.getElementById('configButton').src = "img/settingsGray.svg";
-			document.getElementById('contacts').style.display = "initial";
-			document.getElementById('contactsButton').src = "img/phone-bookBlue.svg";
-			break;
-		case "chatsButton":
-			document.getElementById('chats').style.display = "initial";
-			document.getElementById('chatsButton').src = "img/speechBlue.svg";
-			document.getElementById('config').style.display = "none";
-			document.getElementById('configButton').src = "img/settingsGray.svg";
-			document.getElementById('contacts').style.display = "none";
-			document.getElementById('contactsButton').src = "img/phone-bookGray.svg";
-			break;
-		case "configButton":
-			document.getElementById('chats').style.display = "none";
-			document.getElementById('chatsButton').src = "img/speechGray.svg";
-			document.getElementById('config').style.display = "initial";
-			document.getElementById('configButton').src = "img/settingsBlue.svg";
-			document.getElementById('contacts').style.display = "none";
-			document.getElementById('contactsButton').src = "img/phone-bookGray.svg";
-			break;
-		default:
-			console.log("Error en funcion nav()");
-			break;
+	if(window.matchMedia("(max-width: 767px)").matches)
+	{
+		switch(id){
+			case "contactsButton":
+				document.getElementById('chats').style.display = "none";
+				document.getElementById('chatsButton').src = "img/speechGray.svg";
+				document.getElementById('config').style.display = "none";
+				document.getElementById('configButton').src = "img/settingsGray.svg";
+				document.getElementById('contacts').style.display = "initial";
+				document.getElementById('contactsButton').src = "img/phone-bookBlue.svg";
+				break;
+			case "chatsButton":
+				document.getElementById('chats').style.display = "initial";
+				document.getElementById('chatsButton').src = "img/speechBlue.svg";
+				document.getElementById('config').style.display = "none";
+				document.getElementById('configButton').src = "img/settingsGray.svg";
+				document.getElementById('contacts').style.display = "none";
+				document.getElementById('contactsButton').src = "img/phone-bookGray.svg";
+				break;
+			case "configButton":
+				document.getElementById('chats').style.display = "none";
+				document.getElementById('chatsButton').src = "img/speechGray.svg";
+				document.getElementById('config').style.display = "initial";
+				document.getElementById('configButton').src = "img/settingsBlue.svg";
+				document.getElementById('contacts').style.display = "none";
+				document.getElementById('contactsButton').src = "img/phone-bookGray.svg";
+				break;
+			default:
+				console.log("Error en funcion nav()");
+				break;
+		}
 	}
+}
+
+function changePassword(){
+	document.getElementById('changePassword').style.display = "block";
+}
+
+function savePassword(){
+	document.getElementById('changePassword').style.display = "none";
 }
 
 
