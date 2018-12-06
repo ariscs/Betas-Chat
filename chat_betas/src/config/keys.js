b7ed11fe8219fe65e03fd09bae6bc9ef876fc73f
Eladio Rocha Vizcaino <39393035+EladioRocha@users.noreply.github.com>
2018-12-06 14:04:32 -0600

    Chat completo de alfas
    Chat completo, incluye las vistas

---
+++ chat_betas/src/config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    dbSettings: {
+        host: 'us-cdbr-iron-east-01.cleardb.net',
+        user: 'bed2011c559d4e',
+        password: '4f74acbe',
+        port: '3306',
+        database: 'heroku_299111733ef37b5'
+    },
+}
