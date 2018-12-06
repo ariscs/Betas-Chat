b4e8c9f7aac5d31347f6acede6708d296674a808
Eladio Rocha Vizcaino <39393035+EladioRocha@users.noreply.github.com>
2018-12-06 12:53:35 -0600

    Add files via upload

---
+++ Betas-Chat-master/Chat/Public/js/funciones.js
@@ -0,0 +1,39 @@
+function nav(id){
+	switch(id){
+		case "contactsButton":
+			document.getElementById('chats').style.display = "none";
+			document.getElementById('chatsButton').src = "img/speechGray.svg";
+			document.getElementById('config').style.display = "none";
+			document.getElementById('configButton').src = "img/settingsGray.svg";
+			document.getElementById('contacts').style.display = "initial";
+			document.getElementById('contactsButton').src = "img/phone-bookBlue.svg";
+			break;
+		case "chatsButton":
+			document.getElementById('chats').style.display = "initial";
+			document.getElementById('chatsButton').src = "img/speechBlue.svg";
+			document.getElementById('config').style.display = "none";
+			document.getElementById('configButton').src = "img/settingsGray.svg";
+			document.getElementById('contacts').style.display = "none";
+			document.getElementById('contactsButton').src = "img/phone-bookGray.svg";
+			break;
+		case "configButton":
+			document.getElementById('chats').style.display = "none";
+			document.getElementById('chatsButton').src = "img/speechGray.svg";
+			document.getElementById('config').style.display = "initial";
+			document.getElementById('configButton').src = "img/settingsBlue.svg";
+			document.getElementById('contacts').style.display = "none";
+			document.getElementById('contactsButton').src = "img/phone-bookGray.svg";
+			break;
+		default:
+			console.log("Error en funcion nav() id invalido");
+			break;
+	}
+}
+
+function changePassword(){
+	document.getElementById('changePassword').style.display = "block";
+}
+
+function savePassword(){
+	document.getElementById('changePassword').style.display = "none";
+}
