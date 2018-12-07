d00b86aefa2cc56246b7f9bec0863e4f6b40d05a
Carlos Sanchez <csanchez50@alumnos.uaq.mx>
2018-12-07 11:03:06 -0600

    dfadfs

--- chat_betas/src/assets/js/funciones_jq.js
+++ chat_betas/src/assets/js/funciones_jq.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //Funciones Jquery
 $( ".toggle-switch" ).click(function() {
 	
@@ -112,3 +113,31 @@ $( "#AddUser-img" ).click(function() {
 $( "#close-img" ).click(function() {
 	$('.sticky').hide(); 
 });
+=======
+//Funciones Jquery
+$( ".toggle-switch" ).click(function() {
+	
+	if ($("#RepetirContraseña").length) {
+		$('#registro-button').replaceWith('<input type="submit" id="login-button" class ="mandar" value="Login">');
+		$('#RepetirContraseña').remove();
+		$('#MandarCorreo').replaceWith('<input type="text" class="inputDatos" id="Correo" placeholder="Correo electronico">');
+		$('#MandarContraseña').replaceWith('<input type="text" class="inputDatos" id="Contraseña" placeholder="Contraseña">');
+	}else{
+		$('#login-button').replaceWith('<input type="submit" id="registro-button" class ="mandar" value="Registrar">');		
+		$('.mandar').before('<input type="text" class="inputDatos" id="RepetirContraseña" placeholder="Repita la Contraseña">');
+		$('#Correo').replaceWith('<input type="text" class="inputDatos" id="MandarCorreo" placeholder="Correo electronico">');
+		$('#Contraseña').replaceWith('<input type="text" class="inputDatos" id="MandarContraseña" placeholder="Contraseña">');
+	}	
+});
+
+
+
+
+$(document).ready(function() {
+	$("#Principal").mousemove(function(e){
+		var pageX = (e.pageX* -1 /15) ;
+		var pageY = (e.pageY* -1 /15) ;
+		$('#Principal').css("background-position", pageX+"px     "+pageY+"px");
+	});
+});
+>>>>>>> parent of 72bdaf4... Fusion de manera exitosa intento #8953
