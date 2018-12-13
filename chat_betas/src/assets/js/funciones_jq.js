614083e1214ff49a96bcef98a2192c214ff2ed1f
Carlos Sanchez <csanchez50@alumnos.uaq.mx>
2018-12-13 03:24:18 -0600

    Colors edited

--- chat_betas/src/assets/js/funciones_jq.js
+++ chat_betas/src/assets/js/funciones_jq.js
@@ -120,3 +120,47 @@ $( "#AddUser-img" ).click(function() {
 $( "#close-img" ).click(function() {
 	$('.sticky').hide(); 
 });
+
+
+
+JavaScript
+jQuery(function ()
+{
+    jQuery("input[type=file]").change(function() {
+        readURL(this);
+    });
+
+    const readURL = (input) => {
+
+        if (input.files && input.files[0]) {
+            const reader = new FileReader();
+
+            reader.onload = (e) => {
+                jQuery('#logo-img').attr('src', e.target.result)
+                jQuery('#container-logo').css('display', 'block');
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    };
+})
+
+	
+jQuery(function ()
+{
+    jQuery("input[type=file]").change(function() {
+        readURL(this);
+    });
+ 
+    const readURL = (input) => {
+ 
+        if (input.files && input.files[0]) {
+            const reader = new FileReader();
+ 
+            reader.onload = (e) => {
+                jQuery('#logo-img').attr('src', e.target.result)
+                jQuery('#container-logo').css('display', 'block');
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    };
+})
